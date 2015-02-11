d1e0b02d396e5551d602231008849bc2fe038c6a
raymondphua <chrphua@student.avans.nl>
2015-02-11 11:15:32 +0100

    Merge origin/master
    Conflicts:
    	home/home.php

--- header/header.php
+++ header/header.php
@@ -1,14 +1,9 @@
 <a href="../home/home.php"><img src='../header/logo.jpg' alt='Logo' width="200" height="100"></a>
 <br/>
 
-<div class="navbar">
-  <ul>
-    <li><a href="../home/home.php">Home</a></li>
-    <li><a href="../product/product.php">Product</a></li>
-    <li><a href="../about/about.php">About</a></li>
-    <li><a href="../blog/blog.php">Blog</a></li>
-  </ul>
-</div>
+<?php
+    include('../nav/nav.php');
+?>
 
 
 
---
+++ nav/nav.php
@@ -0,0 +1,8 @@
+<div class="navbar">
+  <ul>
+    <li><a href="../home/home.php">Home</a></li>
+    <li><a href="../product/product.php">Product</a></li>
+    <li><a href="../about/about.php">About</a></li>
+    <li><a href="../blog/blog.php">Blog</a></li>
+  </ul>
+</div>

f4bc982b39cd647f13152f22481658632853d9a5
raymondphua <chrphua@student.avans.nl>
2015-03-30 14:34:39 +0200

    dit keer de echte order

--- controller/AddToOrderController.php
+++ controller/AddToOrderController.php
@@ -6,16 +6,47 @@
  * Time: 12:48
  */
 
+session_start();
 
+$username = "";
+if(isset($_SESSION['username'])) {
+    $username = $_SESSION['username'];
+}
 
 include '../model/Database.class.php';
 include '../model/Order.class.php';
+include '../model/User.class.php';
+include '../model/Product.class.php';
 
 include '../view/headerView.php';
 include '../model/Navigation.class.php';
 include '../view/footerView.php';
 
-$user = User::getUser($username);
+if ($username != "") {
+    $user = User::getUser($username);
+    $orderedProducts = array();
 
-$order = Order::CreateOrder($user->getId(), 1, )
+    //fill products array
+    foreach ($_SESSION['cart'] as $cartItem) {
+        $product = Product::getProduct($cartItem);
+        $orderedProducts[] = $product;
+    }
 
+    $order = Order::createOrder($user->getId(), 1, date('Y-m-d H:i:s'), date('Y-m-d H:i:s'), $user->getAddress(), $user->getZip(), $user->getCity(), $user->getCountry(), $orderedProducts);
+
+    if ($order) {
+        /*
+        loadHeader();
+
+        loadFooter();
+        */
+        header("Location: OrderController.php");
+    }
+    else {
+        echo "error";
+    }
+}
+else {
+    echo "U moet eerst inloggen! <br>";
+    echo "<a href='LoginController.php'>Login</a>";
+}
